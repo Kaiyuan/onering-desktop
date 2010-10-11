bd5724d424b41ec4ba422ff7d64a99f4e1845e5f
Qiangning Hong <hongqn@gmail.com>
2010-10-11 19:49:15 +0800

    remove deprecated OneRingView constructor.

--- src/oneringview.cpp
+++ src/oneringview.cpp
@@ -3,31 +3,6 @@
 #include "networkaccessmanager.h"
 #include "jsapi.h"
 
-OneRingView::OneRingView(QWidget *parent, Qt::WindowFlags f)
-	: QWebView(parent), contextMenuEnabled(false)
-{
-	setWindowFlags(f);
-
-	QNetworkAccessManager *oldManager = page()->networkAccessManager();
-	NetworkAccessManager *newManager = new NetworkAccessManager(this, oldManager);
-	page()->setNetworkAccessManager(newManager);
-
-#ifdef DEBUG
-	connect(this, SIGNAL(urlChanged(const QUrl &)),
-		this, SLOT(printCurrentUrl(const QUrl &)));
-#endif
-
-	connect(this, SIGNAL(titleChanged(const QString &)),
-		this, SLOT(setWindowTitle(const QString &)));
-
-	JsApi *jsapi = new JsApi(this);
-	jsapi->setWebView(this);
-	jsapi->setWindow(this);
-
-	this->setAttribute(Qt::WA_DeleteOnClose, true);
-
-}
-
 OneRingView::OneRingView(const QUrl &url, int width, int height, QVariantMap &props)
 	: QWebView(), contextMenuEnabled(false)
 {

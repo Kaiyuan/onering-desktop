4938fffa50e1ed03e921e6fb717f31c73d045276
Qiangning Hong <hongqn@gmail.com>
2011-01-28 23:08:51 +0800

    refactor: move register onering app to oneringapp.cpp

--- src/oneringapp.cpp
+++ src/oneringapp.cpp
@@ -1,5 +1,6 @@
 #include <QDebug>
 #include <QFile>
+#include <onering.h>
 #include "oneringapp.h"
 #include "debugger.h"
 
@@ -17,7 +18,7 @@ int onering_app_init()
 	return 0;
 }
 
-onering_response_handle_t onering_app(const char *appname, const char* method, const char* path, const char* body, const char **response, int *response_len)
+static onering_response_handle_t onering_app(const char *appname, const char* method, const char* path, const char* body, const char **response, int *response_len)
 {
 	if (strcmp(method, "GET") == 0 && strcmp(path, "/onering.js") == 0 && onering_js) {
 		*response = onering_js->constData();
@@ -31,6 +32,11 @@ onering_response_handle_t onering_app(const char *appname, const char* method, c
 	return 0;
 }
 
-void onering_app_free_response(const char *appname, onering_response_handle_t response_handle)
+static void onering_app_free_response(const char *appname, onering_response_handle_t response_handle)
 {
+}
+
+void register_onering_app(const char* appname)
+{
+	onering_register_app(appname, &onering_app, &onering_app_free_response);
 }

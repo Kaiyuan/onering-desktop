0ed4d37aef345f63dab3d59a312d4812db1dd931
Qiangning Hong <hongqn@gmail.com>
2011-09-02 15:09:10 +0800

    add C version of test program

---
+++ qtcore/test/test.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <onering.h>
+
+onering_response_handle_t test_app(const char* appname, const char* method, const char* path, const char* body, const char** response, int* response_len)
+{
+	if (strcmp(path, "/init") == 0) {
+		*response = "{\"width\": 400, \"height\": 300, \"url\": \"/\"}";
+	} else {
+		*response = "<html><head><script src='onering://onering/onering.js'></script></head><body><p>Hello, World!</p></body></html>";
+	}
+	*response_len = strlen(*response);
+	return (onering_response_handle_t)0;
+}
+
+void free_response(const char* appname, onering_response_handle_t response_handle)
+{
+}
+
+int main()
+{
+	onering_register_app("test", &test_app, &free_response);
+	onering_loop("test");
+	return 0;
+}

244cd0ef0d5bacc032c11110037781a99f6834c9
Qiangning Hong <hongqn@gmail.com>
2011-11-20 22:17:45 +0800

    add Window and controller

---
+++ cores/webkitcore/OneRing/OneRingWindowController.m
@@ -0,0 +1,24 @@
+//
+//  OneRingWindowController.m
+//  OneRing
+//
+//  Created by hongqn on 11/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "OneRingWindowController.h"
+
+@implementation OneRingWindowController
+@synthesize theWebView;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end

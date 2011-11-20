c930c1fcc5ba0b0a3a5af1265bfcbe8b7ef01154
Qiangning Hong <hongqn@gmail.com>
2011-11-20 22:54:49 +0800

    version

---
+++ cores/webkitcore/OneRing/core.c
@@ -0,0 +1,14 @@
+//
+//  core.c
+//  
+//
+//  Created by hongqn on 11/20/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <onering.h>
+
+const char* onering_version()
+{
+    return "WebKitCore 1.0.0";
+}
